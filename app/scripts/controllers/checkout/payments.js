943cb0561ca6609b23966137926794cc6b4d6310
Luis <luis19prz@gmail.com>
2017-02-03 12:58:43 -0500

    remaining fix and update quotation not including client on payments

--- app/scripts/controllers/checkout/payments.js
+++ app/scripts/controllers/checkout/payments.js
@@ -229,7 +229,7 @@ function CheckoutPaymentsCtrl(
   function updateVMQuoatation(newQuotation){
     vm.quotation.ammountPaid = newQuotation.ammountPaid;
     vm.quotation.paymentGroup = newQuotation.paymentGroup;
-    vm.quotation.Client = newQuotation.Client || vm.quotation.Client;            
+    //vm.quotation.Client = newQuotation.Client || vm.quotation.Client;            
     vm.quotation = setQuotationTotalsByGroup(vm.quotation);
     delete vm.activeMethod;
   }
