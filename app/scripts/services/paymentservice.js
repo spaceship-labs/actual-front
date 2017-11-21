de117d98040691719139f1597013950aed4966e3
Luis <luis19prz@gmail.com>
2017-11-21 18:11:34 -0500

    disabled 18msi

--- app/scripts/services/paymentservice.js
+++ app/scripts/services/paymentservice.js
@@ -131,7 +131,7 @@
 
       {
         card:{label:'Banamex', value:'banamex'},
-        paymentTypes: ['3-msi-banamex','6-msi-banamex','9-msi-banamex', '12-msi-banamex','13-msi','18-msi'],
+        paymentTypes: ['3-msi-banamex','6-msi-banamex','9-msi-banamex', '12-msi-banamex','13-msi'/*,'18-msi'*/],
         storesTypes:['home','studio','proyectos'],
         terminal: {label:'Banamex', value:'banamex'}
       },
