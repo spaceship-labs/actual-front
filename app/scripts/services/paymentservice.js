4436c3cbd1708a794573448100f5379fa797a9e7
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-07-12 13:50:01 -0500

    last merged with feature/3

--- app/scripts/services/paymentservice.js
+++ app/scripts/services/paymentservice.js
@@ -1,13 +1,7 @@
 (function() {
-<<<<<<< HEAD
-  "use strict";
-
-  angular.module("actualApp").factory("paymentService", paymentService);
-=======
   'use strict';
 
   angular.module('actualApp').factory('paymentService', paymentService);
->>>>>>> feature/3
 
   function paymentService(
     api,
@@ -20,42 +14,18 @@
   ) {
     var paymentOptions = paymentOptionsConfig.getAll();
     var CLIENT_BALANCE_GROUP_INDEX = 0;
-    var CLIENT_BALANCE_TYPE = "client-balance";
+    var CLIENT_BALANCE_TYPE = 'client-balance';
 
     var statusTypes = {
-      CANCELED: 'canceled'
+      CANCELED: 'canceled',
     };
 
     var currencyTypes = {
-      USD: "usd",
-      MXN: "mxn"
+      USD: 'usd',
+      MXN: 'mxn',
     };
 
     var types = {
-<<<<<<< HEAD
-      CLIENT_BALANCE: "client-balance",
-      CASH: "cash",
-      CASH_USD: "cash-usd",
-      CHEQUE: "cheque",
-      DEPOSIT: "deposit",
-      TRANSFER: "transfer",
-      TRANSFER_USD: "transfer-usd",
-      CREDIT_CARD: "credit-card",
-      DEBIT_CARD: "debit-card",
-      SINGLE_PAYMENT_TERMINAL: "single-payment-terminal",
-      MSI_3: "3-msi",
-      MSI_6: "6-msi",
-      MSI_9: "9-msi",
-      MSI_12: "12-msi",
-
-      MSI_3_BANAMEX: "3-msi-banamex",
-      MSI_6_BANAMEX: "6-msi-banamex",
-      MSI_9_BANAMEX: "9-msi-banamex",
-      MSI_12_BANAMEX: "12-msi-banamex",
-
-      MSI_13: "13-msi",
-      MSI_18: "18-msi"
-=======
       CLIENT_BALANCE: 'client-balance',
       CASH: 'cash',
       CASH_USD: 'cash-usd',
@@ -78,8 +48,7 @@
       MSI_12_BANAMEX: '12-msi-banamex',
 
       MSI_13: '13-msi',
-      MSI_18: '18-msi'
->>>>>>> feature/3
+      MSI_18: '18-msi',
     };
 
     var service = {
@@ -106,30 +75,9 @@
       isCardCreditOrDebitPayment: isCardCreditOrDebitPayment,
       isCanceled: isCanceled,
       isClientBalanceOrEwalletPayment: isClientBalanceOrEwalletPayment,
-      mapStatusType: mapStatusType
+      mapStatusType: mapStatusType,
     };
 
-<<<<<<< HEAD
-    function getAmountMXN(amount, exchangeRate) {
-      return amount * exchangeRate;
-    }
-
-    function isCardPayment(payment) {
-      return (
-        payment.msi ||
-        payment.type === types.CREDIT_CARD ||
-        payment.type === types.DEBIT_CARD ||
-        payment.type === types.SINGLE_PAYMENT_TERMINAL
-      );
-    }
-
-    function isCardCreditOrDebitPayment(payment) {
-      return (
-        payment.type === types.CREDIT_CARD || payment.type === types.DEBIT_CARD
-      );
-    }
-
-=======
     function mapStatusType(status) {
       var mapper = {};
       mapper[statusTypes.CANCELED] = 'Cancelado';
@@ -170,7 +118,6 @@
       );
     }
 
->>>>>>> feature/3
     function isUsdPayment(payment) {
       return payment.currency === currencyTypes.USD;
     }
@@ -193,11 +140,7 @@
       );
     }
 
-<<<<<<< HEAD
-    function isSinglePlaymentTerminal(payment) {
-=======
     function isSinglePaymentTerminal(payment) {
->>>>>>> feature/3
       return (
         payment.type === types.SINGLE_PAYMENT_TERMINAL ||
         payment.type === types.DEBIT_CARD ||
@@ -206,11 +149,6 @@
     }
 
     function getPaymentOptionsByMethod(method) {
-<<<<<<< HEAD
-      console.log("method", method);
-      var STUDIO_MALECON_CODE = "actual_studio_malecon";
-=======
->>>>>>> feature/3
       var options = _.filter(paymentOptions, function(option) {
         var hasPaymentType = false;
         var hasStore = false;
@@ -229,75 +167,36 @@
           hasStore = true;
         }
 
-<<<<<<< HEAD
         return (
           hasStore &&
           hasPaymentType &&
           method.isInternational === option.isInternational
         );
-=======
-        if (
-          hasStore &&
-          hasPaymentType &&
-          method.isInternational === option.isInternational
-        ) {
-          return true;
-        }
-
-        return false;
->>>>>>> feature/3
       });
       return options;
     }
 
     function getPaymentMethodsGroups(params) {
-<<<<<<< HEAD
-      var url = "/paymentgroups";
-=======
       var url = '/paymentgroups';
->>>>>>> feature/3
       return api.$http.post(url, params);
     }
 
     function getPaymentWebMethodsGroups(params) {
-<<<<<<< HEAD
-      var url = "/paymentwebgroups";
-=======
       var url = '/paymentwebgroups';
->>>>>>> feature/3
       return api.$http.post(url, params);
     }
 
     function addPayment(quotationId, params) {
-<<<<<<< HEAD
-      var url = "/payment/add/" + quotationId;
-      return api.$http.post(url, params);
-    }
-
-    function cancelPayment(quotationId, paymentId) {
-      var url = "/payment/cancel/" + quotationId + "/" + paymentId;
-=======
       var url = '/quotation/' + quotationId + '/payments';
       return api.$http.post(url, params);
     }
 
     function cancel(paymentId) {
       var url = '/payment/' + paymentId + '/cancel';
->>>>>>> feature/3
       return api.$http.post(url);
     }
 
     function getPaymentTypeString(payment) {
-<<<<<<< HEAD
-      var type = "1 sola exhibición";
-      if (payment.msi) {
-        type = payment.msi + " meses sin intereses";
-        return type;
-      } else if (isTransferPayment(payment)) {
-        type = "Transferencia";
-      } else if (isDepositPayment(payment)) {
-        type = "Deposito en ventanilla";
-=======
       var type = '1 sola exhibición';
       if (payment.msi) {
         type = payment.msi + ' meses sin intereses';
@@ -306,19 +205,18 @@
         type = 'Transferencia';
       } else if (isDepositPayment(payment)) {
         type = 'Deposito en ventanilla';
->>>>>>> feature/3
       }
 
       switch (payment.type) {
         case types.CASH:
         case types.CASH_USD:
-          type = "Pago de contado";
+          type = 'Pago de contado';
           break;
         case types.EWALLET_TYPE:
-          type = "Monedero electrónico";
+          type = 'Monedero electrónico';
           break;
         case types.CLIENT_BALANCE:
-          type = "Saldo a favor cliente";
+          type = 'Saldo a favor cliente';
           break;
       }
       return type;
@@ -328,19 +226,15 @@
     function updateQuotationClientBalance(quotation, paymentMethodsGroups) {
       var group = paymentMethodsGroups[CLIENT_BALANCE_GROUP_INDEX];
       var balancePaymentMethod = _.findWhere(group.methods, {
-        type: CLIENT_BALANCE_TYPE
+        type: CLIENT_BALANCE_TYPE,
       });
       var balancePayments = _.where(quotation.Payments, {
-        type: CLIENT_BALANCE_TYPE
+        type: CLIENT_BALANCE_TYPE,
       });
       clientService
         .getBalanceById(quotation.Client.id)
         .then(function(res) {
-<<<<<<< HEAD
-          console.log("res", res);
-=======
           console.log('res client balance', res);
->>>>>>> feature/3
           var balance = res.data || 0;
           var description = getClientBalanceDescription(balance);
           if (balancePaymentMethod) {
@@ -358,14 +252,11 @@
         });
     }
 
-<<<<<<< HEAD
-=======
     /*
-    @params 
+    @params
     method {Object}
     quotation {Quotation Object} populated with Client{ Client Object}
     */
->>>>>>> feature/3
     function getMethodAvailableBalance(method, quotation) {
       var EWALLET_TYPE = ewalletService.ewalletType;
       var balance = 0;
@@ -381,17 +272,10 @@
     }
 
     function getClientBalanceDescription(balance) {
-<<<<<<< HEAD
-      var description = "";
-      var balanceRounded = commonService.roundCurrency(balance, { up: false });
-      var balanceStr = $filter("currency")(balanceRounded);
-      description = "Saldo disponible: " + balanceStr + " MXN";
-=======
       var description = '';
       var balanceRounded = commonService.roundCurrency(balance, { up: false });
       var balanceStr = $filter('currency')(balanceRounded);
       description = 'Saldo disponible: ' + balanceStr + ' MXN';
->>>>>>> feature/3
       return description;
     }
 
