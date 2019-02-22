4a5a25edeb754797b98e7abdb761269f334c0afb
Luis <luis19prz@gmail.com>
2019-02-22 11:42:29 -0500

    profeco link only for logged users

--- app/scripts/envconfig.js
+++ app/scripts/envconfig.js
@@ -2,6 +2,6 @@
 
  angular.module('envconfig', [])
 
-.constant('ENV', {name:'dev',apiEndpoint:'http://localhost:1337',cdnUrl:'https://d116li125og699.cloudfront.net',adminUrl:'http://localhost:3000',tokenPrefix:'dev'})
+.constant('ENV', {name:'production',apiEndpoint:'http://13.58.136.42:8082',cdnUrl:'https://d116li125og699.cloudfront.net',adminUrl:'http://admin.miactual.com',tokenPrefix:'production'})
 
 ;
