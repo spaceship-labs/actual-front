450982b3981d7e7222804df538449b98e842c22f
brandonNavarrete <brandon@spaceshiplabs.com>
2019-03-06 17:18:00 -0500

    status asociate ewallet

--- app/scripts/envconfig.js
+++ app/scripts/envconfig.js
@@ -2,6 +2,6 @@
 
  angular.module('envconfig', [])
 
-.constant('ENV', {name:'staged',apiEndpoint:'https://stagedapi.miactual.com',adminUrl:'http://stagedadmin.miactual.com',tokenPrefix:'sandbox'})
+.constant('ENV', {name:'dev',apiEndpoint:'http://localhost:1337',cdnUrl:'https://d116li125og699.cloudfront.net',adminUrl:'http://localhost:3000',tokenPrefix:'dev'})
 
 ;
