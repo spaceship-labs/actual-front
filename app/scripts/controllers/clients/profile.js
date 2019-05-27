2bdb3ad0c0bde56815c7d69170d2af97df3ac51f
Brandon Navarrete <brandon@spaceshiplabs.com>
2019-05-27 16:40:13 -0500

    process ewallet

--- app/scripts/controllers/clients/profile.js
+++ app/scripts/controllers/clients/profile.js
@@ -160,29 +160,18 @@ function ClientProfileCtrl(
   function asociateEwallet(type) {
     return showAsociateEwalletDialog(type)
       .then(function(ewallet) {
-        console.log('client', vm.client);
-        console.log('ewallet', vm.ewallet);
-        if (vm.client.EwalletContract) {
-          if (vm.client.id === ewallet.Client) {
-            vm.ewallet = ewallet;
-            vm.ewallet.amount = parseFloat(vm.ewallet.amount.toFixed(2));
-            vm.ewallet.mxnAmount = paymentService.pointsToMXN(
-              vm.ewallet.amount,
-              vm.ewallet.exchangeRate
-            );
-            vm.asociateEwalletStatus = true;
-            if (type != 'show') {
-              vm.asociateStatus = false;
-              vm.clientEwallet = true;
-              dialogService.showDialog(
-                'Monedenero relacionado satisfactoriamente'
-              );
-            }
-          } else {
-            dialogService.showDialog('Monedero no corresponde al Cliente');
-          }
-        } else {
-          dialogService.showDialog('Cliente no tiene un contrato activo');
+        console.log('ewallet', ewallet);
+        vm.ewallet = ewallet;
+        vm.ewallet.amount = parseFloat(vm.ewallet.amount.toFixed(2));
+        vm.ewallet.mxnAmount = paymentService.pointsToMXN(
+          vm.ewallet.amount,
+          vm.ewallet.exchangeRate
+        );
+        vm.asociateEwalletStatus = true;
+        if (type != 'show') {
+          vm.asociateStatus = false;
+          vm.clientEwallet = true;
+          dialogService.showDialog('Monedenero relacionado satisfactoriamente');
         }
       })
       .catch(function(err) {
