d9f29014d20d7647da5477e8570ffc8585ec6027
Gibran <gibran@spaceshiplabs.com>
2023-12-08 15:32:38 -0500

    Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -504,7 +504,7 @@ module.exports = function(grunt) {
         constants: {
           ENV: {
             name: 'dev',
-            apiEndpoint: 'http://localhost:1337',
+            apiEndpoint: 'http://localhost:1341',
             cdnUrl: 'https://d116li125og699.cloudfront.net',
             adminUrl: 'http://localhost:3000',
             tokenPrefix: 'dev'
